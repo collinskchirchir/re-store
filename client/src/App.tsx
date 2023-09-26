65f3d69d136c1cf1cd2ccf3a7f2df23a1af284a3
Collins Chirchir <collinskchirchir@gmail.com>
2023-09-26 19:43:34 +0300

    products component skeleton

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,22 @@
 
 function App() {
-  
+  const products = [
+    {name: 'product1', price: 100.00},
+    {name: 'product2', price: 200.00},
+    {name: 'product3', price: 200.00},
+  ]
 
   return (
     <>
+    <div className="app">
       <h1>Re-Store</h1>
+      <ul>
+        {products.map((item, index) => (
+          <li key={index}>{item.name} - {item.price}</li>
+        ))}
+      </ul>
+
+    </div>
     </>
   )
 }
