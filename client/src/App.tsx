6a66e4692e206fe410656eff37137cc1c272b66f
Collins Chirchir <collinskchirchir@gmail.com>
2023-09-26 20:09:16 +0300

    useEffect hook

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,15 @@
-import {useState} from "react"
+import {useState, useEffect} from "react"
 function App() {
   const [products, SetProducts] = useState([
     {name: 'product1', price: 100.00},
     {name: 'product2', price: 200.00},
   ]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/products')
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }, [])
 
   function addProduct(){
     SetProducts(prevState => [...prevState, {name: 'product' + (prevState.length + 1), price: (prevState.length * 100) + 100}])
