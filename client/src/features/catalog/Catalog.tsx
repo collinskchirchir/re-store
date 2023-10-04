3583b43df541a96abea779e8d8ad61025a70901c
Collins Chirchir <collinskchirchir@gmail.com>
2023-10-04 21:15:11 +0300

    added images

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,3 +1,4 @@
+import { Avatar, List, ListItem, ListItemAvatar, ListItemText, Button } from "@mui/material";
 import { Products } from "../../app/models/products"
 
 interface Props {
@@ -7,13 +8,22 @@ interface Props {
 export default function Catalog({products, addProduct}: Props){
    return (
       <>
-      <h1>Catalog</h1>
-      <ul>
-        {products.map((product) => (
-          <li key={product.id}>{product.name} - {product.price}</li>
-        ))}
-      </ul>
-      <button onClick={addProduct}>Add Product</button>
+      <List>
+         
+            {products.map((product) => (
+               <ListItem key={product.id}>
+                  <ListItemAvatar>
+                     <Avatar src={product.pictureUrl}/>
+                  </ListItemAvatar>
+                  <ListItemText>
+                     {product.name} - {product.price}
+                  </ListItemText>
+               </ListItem>
+            ))}
+         
+      </List>
+      
+      <Button variant="contained" onClick={addProduct}>Add Product</Button>
       </>
    )
 }
