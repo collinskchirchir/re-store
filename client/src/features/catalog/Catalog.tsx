c703a234e9b96f811d42496dd1e3b3f28df7547d
Collins Chirchir <collinskchirchir@gmail.com>
2023-10-17 14:22:31 +0300

    centralizing axios requests

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,3 +1,4 @@
+import agent from "../../app/api/agent";
 import { Products } from "../../app/models/products"
 import ProductList from "./ProductList";
 import {useState, useEffect} from "react"
@@ -7,9 +8,10 @@ export default function Catalog(){
    const [products, SetProducts] = useState<Products[]>([]);
 
    useEffect(() => {
-      fetch('http://localhost:5000/api/products')
-      .then(res => res.json())
-      .then(data => SetProducts(data))
+      // fetch('http://localhost:5000/api/products')
+      // .then(res => res.json())
+      // .then(data => SetProducts(data))
+      agent.Catalog.list().then(products => SetProducts(products))
    }, [])
 
    return (
--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,8 +1,8 @@
 import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from "@mui/material";
-import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Products } from "../../app/models/products";
+import agent from "../../app/api/agent";
 
 export default function ProductDetails() {
    const {id} = useParams<{id: string}>();
@@ -10,8 +10,9 @@ export default function ProductDetails() {
    const [loading, setLoading] = useState(true)
    
    useEffect(() => {
-      axios.get(`http://localhost:5000/api/products/${id}`)
-      .then(resp => setProduct(resp.data))
+    //   axios.get(`http://localhost:5000/api/products/${id}`)
+      id && agent.Catalog.details(parseInt(id))
+      .then(resp => setProduct(resp))
       .catch(error => console.log(error))
       .finally(() => setLoading(false))
    }, [id])
