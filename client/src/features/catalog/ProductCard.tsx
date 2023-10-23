11eb913cbd30d7be3dd1f6540e75e8f428e1eb19
Collins Chirchir <collinskchirchir@gmail.com>
2023-10-23 20:20:57 +0300

    adding axios methods for the basket

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -1,11 +1,21 @@
 import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Typography } from "@mui/material";
 import { Products } from "../../app/models/products";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import agent from "../../app/api/agent";
+import { LoadingButton } from "@mui/lab";
 
 interface Props {
    product: Products;
 }
 export default function ProductCard({product}: Props) {
+   const [loading, setLoading] = useState(false);
+   function handleAddItem(productId: number){
+      setLoading(true);
+      agent.Basket.addItem(productId)
+         .catch(error => console.log(error))
+         .finally(() => setLoading(false))
+   }
    return (
       <Card >
          <CardHeader
@@ -33,7 +43,10 @@ export default function ProductCard({product}: Props) {
             </Typography>
          </CardContent>
          <CardActions>
-            <Button size="small">Add to Cart</Button>
+            <LoadingButton 
+               loading={loading} 
+               onClick={()=> handleAddItem(product.id)} 
+               size="small">Add to Cart</LoadingButton>
             <Button component={Link} to={`/catalog/${product.id}`} size="small">View</Button>
          </CardActions>
       </Card>
