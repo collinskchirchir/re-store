0720eefcdd9bfdfd308ffc31143d6b6e2d12b3b9
Collins Chirchir <collinskchirchir@gmail.com>
2023-10-26 20:01:48 +0300

    app initialization - fetching basket on app start

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,12 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "../../features/catalog/Header";
 import { Container, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import { useStoreContext } from "../context/StoreContext";
+import { getCookie } from "../util/util";
+import agent from "../api/agent";
+import LoadingComponents from "./LoadingComponent";
 
 
 function App() {
+  const {setBasket} = useStoreContext();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const buyerId = getCookie('buyerId');
+    if (buyerId) {
+      agent.Basket.get()
+        .then(basket => setBasket(basket))
+        .catch(error => console.log(error))
+        .finally(() => setLoading(false))
+    }
+  }, [])
+
   const [darkMode, setDarkMode] = useState(false);
   const paletteType = darkMode ? 'dark' : 'light';
   const theme = createTheme({
@@ -21,6 +38,7 @@ function App() {
     setDarkMode(!darkMode)
   }
     
+  if (loading) return <LoadingComponents message="initializing app..." />
 
   return (
     <>
