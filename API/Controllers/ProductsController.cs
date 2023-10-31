bd98763e429e6e8d20c394de6ce87778dc67270a
Collins Chirchir <collinskchirchir@gmail.com>
2023-10-31 21:52:59 +0300

    adding sort to the API

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 using API.Data;
 using API.Entities;
+using API.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,9 +16,15 @@ namespace API.Controllers
           _context = context;           
         }
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts()
+        public async Task<ActionResult<List<Product>>> GetProducts(string orderBy)
         {
-            return await _context.Products.ToListAsync();
+            var query = _context.Products
+              .Sort(orderBy)
+              .AsQueryable();
+                     
+
+            return await query.ToListAsync();
+
         }
         [HttpGet("{id}")] // api/products/3
         public async Task<ActionResult<Product>> GetProduct(int id)
