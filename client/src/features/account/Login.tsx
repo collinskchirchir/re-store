e26d6ac413643afcc591afaea40e71b1c7efd0a3
Collins Chirchir <collinskchirchir@gmail.com>
2023-12-03 16:29:30 +0300

    using controlled component

--- client/src/features/account/Login.tsx
+++ client/src/features/account/Login.tsx
@@ -9,17 +9,24 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { Paper } from '@mui/material';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import agent from '../../app/api/agent';
 
 
 export default function Login() {
-   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+   const [values, setValues] = useState({
+      username: '',
+      password: ''
+   })
+   const handleSubmit = (event: any) => {
       event.preventDefault();
-      const data = new FormData(event.currentTarget);
-      console.log({
-         email: data.get('email'),
-         password: data.get('password'),
-      });
+      agent.Account.login(values);
    };
+
+   function handleInputChange(event: any) {
+      const { name, value } = event.target;
+      setValues({...values, [name]: value})
+   }
 
    return (
 
@@ -36,25 +43,24 @@ export default function Login() {
          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
             <TextField
                margin="normal"
-               required
                fullWidth
-               id="email"
-               label="Email Address"
-               name="email"
-               autoComplete="email"
+               label="Username"
+               name="username" 
                autoFocus
+               onChange={handleInputChange}
+               value={values.username}
             />
             <TextField
                margin="normal"
-               required
                fullWidth
                name="password"
                label="Password"
                type="password"
-               id="password"
                autoComplete="current-password"
+               onChange={handleInputChange}
+               value={values.password}
             />
-            
+
             <Button
                type="submit"
                fullWidth
@@ -63,9 +69,9 @@ export default function Login() {
             >
                Sign In
             </Button>
-            <Grid container>               
+            <Grid container>
                <Grid item>
-                  <Link to='/registe r'>
+                  <Link to='/register'>
                      {"Don't have an account? Sign Up"}
                   </Link>
                </Grid>
