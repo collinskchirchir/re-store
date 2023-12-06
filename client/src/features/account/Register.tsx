9290fd243752036148c33608350836a2906a5af6
Collins Chirchir <collinskchirchir@gmail.com>
2023-12-06 15:09:01 +0300

    adding registration validators

--- client/src/features/account/Register.tsx
+++ client/src/features/account/Register.tsx
@@ -1,111 +1,131 @@
+/* eslint-disable  @typescript-eslint/no-explicit-any */
 import Avatar from "@mui/material/Avatar";
 import TextField from "@mui/material/TextField";
 import Grid from "@mui/material/Grid";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import { Alert, AlertTitle, List, ListItem, ListItemText, Paper } from "@mui/material";
+import { Paper } from "@mui/material";
 import Box from "@mui/material/Box";
 import { Link, useNavigate } from "react-router-dom";
 import { LoadingButton } from "@mui/lab";
 import { useForm } from "react-hook-form";
-import { useAppDispatch } from "../../app/store/configureStore";
 import agent from "../../app/api/agent";
-import { useState } from "react";
+import { toast } from "react-toastify";
 
 export default function Register() {
-   const [validationErrors, setValidationErrors] = useState([]);
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { isSubmitting, errors, isValid },
+  } = useForm({
+    mode: "onTouched",
+  });
 
-   const {
-      register,
-      handleSubmit,
-      formState: { isSubmitting, errors, isValid },
-   } = useForm({
-      mode: "onTouched",
-   });
-   
-   return (
-      <Container
-         component={Paper}
-         maxWidth="sm"
-         sx={{
-         display: "flex",
-         flexDirection: "column",
-         alignItems: "center",
-         p: 4,
-         }}
+  function handleApiErrors(errors: any) {
+    console.log(errors);
+    if (errors) {
+      errors.forEach((error: string) => {
+        if (error.includes("Password")) {
+          setError("password", { message: error });
+        } else if (error.includes("Email")) {
+          setError("email", { message: error });
+        } else if (error.includes("Username")) {
+          setError("username", { message: error });
+        }
+      });
+    }
+  }
+  return (
+    <Container
+      component={Paper}
+      maxWidth="sm"
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        p: 4,
+      }}
+    >
+      <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+        <LockOutlinedIcon />
+      </Avatar>
+      <Typography component="h1" variant="h5">
+        Register
+      </Typography>
+      <Box
+        component="form"
+        onSubmit={handleSubmit((data) =>
+          agent.Account.register(data)
+            .then(() => {
+              toast.success("Registration successful - you can now login");
+              navigate("/login");
+            })
+            .catch((error) => handleApiErrors(error))
+        )}
+        noValidate
+        sx={{ mt: 1 }}
       >
-         <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-         <LockOutlinedIcon />
-         </Avatar>
-         <Typography component="h1" variant="h5">
-         Register
-         </Typography>
-         <Box
-         component="form"
-         onSubmit={handleSubmit(handleSubmit(data => agent.Account.register(data)
-            .catch(error => setValidationErrors(error))))}
-         noValidate
-         sx={{ mt: 1 }}
-         >
-            {validationErrors.length > 0 &&
-            <Alert severity="error">
-               <AlertTitle>Validation Errors</AlertTitle>
-               <List>
-                  {validationErrors.map(error => (
-                     <ListItem key={error}>
-                        <ListItemText>{error}</ListItemText>
-                     </ListItem>
-                  ))
+        <TextField
+          margin="normal"
+          fullWidth
+          label="Username"
+          autoFocus
+          {...register("username", { required: "Username is required" })}
+          error={!!errors.username}
+          helperText={errors?.username?.message as string}
+        />
+        <TextField
+          margin="normal"
+          fullWidth
+          label="Email"
+          {...register("email", {
+            required: "Email is required",
+            // regex email pattern => disclaimer: (cant handle **@domain.co.ke) extra dots
+            pattern: {
+              value: /^\w+[\w-.]*@\w+((-\w+)|(\w*))\.[a-z]{2,3}$/,
+              message: "not a valid email address",
+            },
+          })}
+          error={!!errors.email}
+          helperText={errors?.email?.message as string}
+        />
+        <TextField
+          margin="normal"
+          fullWidth
+          label="Password"
+          type="password"
+          {...register("password", {
+            required: "Password is required",
+            // regex password pattern microsoft
+            pattern: {
+              value:
+                /(?=^.{6,10}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\s).*$/,
+              message: "password does no meet complexity requirements",
+            },
+          })}
+          error={!!errors.password}
+          helperText={errors?.password?.message as string}
+        />
 
-                  }
-               </List>  
-            </Alert>
-         }
-         <TextField
-            margin="normal"
-            fullWidth
-            label="Username"
-            autoFocus
-            {...register("username", { required: "Username is required" })}
-            error={!!errors.username}
-            helperText={errors?.username?.message as string}
-         />
-         <TextField
-            margin="normal"
-            fullWidth
-            label="Email"
-            {...register("email", { required: "Email is required" })}
-            error={!!errors.email}
-            helperText={errors?.email?.message as string}
-         />
-         <TextField
-            margin="normal"
-            fullWidth
-            label="Password"
-            type="password"
-            {...register("password", { required: "Password is required" })}
-            error={!!errors.password}
-            helperText={errors?.password?.message as string}
-         />
-         
-         
-         <LoadingButton
-            disabled={!isValid}
-            loading={isSubmitting}
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-         >
-            Register
-         </LoadingButton>
-         <Grid container>
-            <Grid item>
-               <Link to="/login">{"Already have an account? Sign In"}</Link>
-            </Grid>
-         </Grid>
-         </Box>
-      </Container>
-   );
-   }
+        <LoadingButton
+          disabled={!isValid}
+          loading={isSubmitting}
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Register
+        </LoadingButton>
+        <Grid container>
+          <Grid item>
+            <Link to="/login">{"Already have an account? Sign In"}</Link>
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
+  );
+}
