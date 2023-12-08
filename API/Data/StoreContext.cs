324ddbeb72de7d6d29aa8f85de6d177f67ac7fd4
Collins Chirchir <collinskchirchir@gmail.com>
2023-12-08 15:43:03 +0300

    Creating order entities

--- API/Data/StoreContext.cs
+++ API/Data/StoreContext.cs
@@ -1,4 +1,5 @@
 using API.Entities;
+using API.Entities.OrderAggregate;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -11,7 +12,8 @@ namespace API.Data
         {
         }
         public DbSet<Product> Products { get; set; }
-        public DbSet<Basket> Baskets {get; set;}
+        public DbSet<Basket> Baskets { get; set;}
+        public DbSet<Order> Orders { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
