c7decb766b40603ee66b6d35109e893ec6338247
Collins Chirchir <collinskchirchir@gmail.com>
2023-12-14 10:35:06 +0300

    updating the Review component

--- client/src/features/basket/BasketPage.tsx
+++ client/src/features/basket/BasketPage.tsx
@@ -1,94 +1,32 @@
-import { Box, Button, Grid, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
-import { Add, Delete, Remove } from "@mui/icons-material";
-import { LoadingButton } from "@mui/lab";
+import { Button, Grid, Typography } from "@mui/material";
 import BasketSummary from "./BasketSummary";
 import { Link } from "react-router-dom";
-import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
-import { addBasketItemAsync, removeBasketItemAsync } from "./basketSlice";
+import { useAppSelector } from "../../app/store/configureStore";
+import BasketTable from "./BasketTable";
 
 export default function BasketPage() {
   //  const {basket, setBasket, removeItem} = useStoreContext();
-  const {basket, status} = useAppSelector(state => state.basket);
-  const dispatch = useAppDispatch();
-  
-  
+  const { basket } = useAppSelector((state) => state.basket);
 
-   if(!basket) return <Typography variant="h3">Your basket is Empty!</Typography>
-   return(
-      <>
-        <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }}>
-          <TableHead>
-            <TableRow>
-              <TableCell>Product</TableCell>
-              <TableCell align="right">Price</TableCell>
-              <TableCell align="center">Quantity</TableCell>
-              <TableCell align="right">Subtotal</TableCell>
-              <TableCell align="right"></TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {basket.items.map((item) => (
-              <TableRow
-                key={item.productId}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  <Box display='flex' alignItems='center'>
-                    <img src={item.pictureUrl} alt={item.name} style={{height: 50, marginRight: 20}}/>
-                    <span>{item.name}</span>                  
-                  </Box>
-                </TableCell>
-                <TableCell align="right">${(item.price / 100).toFixed(2)}</TableCell>
-                <TableCell align="center">
-                  <LoadingButton 
-                    loading={status === `pendingRemoveItem${item.productId}rem`} 
-                    color="error" 
-                    onClick={() => dispatch(removeBasketItemAsync({
-                      productId: item.productId, quantity: 1, name: 'rem'
-                      }))}>
-                    <Remove />
-                  </LoadingButton>
-                  {item.quantity}
-                  <LoadingButton 
-                    loading={status === `pendingAddItem${item.productId}`} 
-                    color="secondary" 
-                    onClick={() => dispatch(addBasketItemAsync({productId: item.productId}))}>
-                    <Add />
-                  </LoadingButton>
-                </TableCell>
-                <TableCell align="right">${((item.price)/100 * (item.quantity)).toFixed(2)}</TableCell>
-                <TableCell align="right">
-                <LoadingButton
-                  loading={status === `pendingRemoveItem${item.productId}del`} 
-                  color="error" 
-                  onClick={() => dispatch(removeBasketItemAsync({
-                    productId: item.productId, quantity: item.quantity, name: 'del'
-                    }))}
-                    >
-                      <Delete />
-                    </LoadingButton>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+  if (!basket) return <Typography variant="h3">Your basket is Empty!</Typography>;
+  return (
+    <>
+      <BasketTable items={basket.items} />
       <Grid container>
-              <Grid item xs={6}/>
-              <Grid item xs={6}>
-                  <BasketSummary />
-                  <Button
-                    component={Link}
-                    to='/checkout'
-                    variant='contained'
-                    size="large"
-                    fullWidth
-                  >
-                    Checkout
-                  </Button>
-              </Grid>
+        <Grid item xs={6} />
+        <Grid item xs={6}>
+          <BasketSummary />
+          <Button
+            component={Link}
+            to="/checkout"
+            variant="contained"
+            size="large"
+            fullWidth
+          >
+            Checkout
+          </Button>
+        </Grid>
       </Grid>
     </>
-   )
+  );
 }
