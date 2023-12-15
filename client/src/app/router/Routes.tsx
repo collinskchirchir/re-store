36b0602f5acd3ff9f37c5fd3641533ec50ac9fe7
Collins Chirchir <collinskchirchir@gmail.com>
2023-12-15 23:13:50 +0300

    created orders page

--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -12,27 +12,31 @@ import CheckoutPage from "../../features/checkout/CheckoutPage";
 import Login from "../../features/account/Login";
 import Register from "../../features/account/Register";
 import RequireAuth from "./RequireAuth";
+import Orders from "../../features/orders/Orders";
 
 export const router = createBrowserRouter([
    {
       path: '/',
       element: <App />,
       children: [
-         {element: <RequireAuth />, children: [
-            {path: 'checkout', element: <CheckoutPage />},
-         ]},
-         {path: '', element: <HomePage />},
-         {path: 'catalog', element: <Catalog />},
-         {path: 'catalog/:id', element: <ProductDetails />},
-         {path: 'about', element: <AboutPage />},
-         {path: 'contact', element: <ContactPage />},
-         {path: 'server-error', element: <ServerError />},
-         {path: 'not-found', element: <NotFound />},
-         {path: 'basket', element: <BasketPage />},
-         
-         {path: 'login', element: <Login />},
-         {path: 'register', element: <Register />},
-         {path: '*', element: <Navigate replace to='/not-found'/>}
+         {
+            element: <RequireAuth />, children: [
+               { path: 'checkout', element: <CheckoutPage /> },
+               { path: 'orders', element: <Orders /> },
+            ]
+         },
+         { path: '', element: <HomePage /> },
+         { path: 'catalog', element: <Catalog /> },
+         { path: 'catalog/:id', element: <ProductDetails /> },
+         { path: 'about', element: <AboutPage /> },
+         { path: 'contact', element: <ContactPage /> },
+         { path: 'server-error', element: <ServerError /> },
+         { path: 'not-found', element: <NotFound /> },
+         { path: 'basket', element: <BasketPage /> },
+
+         { path: 'login', element: <Login /> },
+         { path: 'register', element: <Register /> },
+         { path: '*', element: <Navigate replace to='/not-found' /> }
       ]
    }
 ])
